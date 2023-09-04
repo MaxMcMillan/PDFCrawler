c32c4bd1af900f7811c0144884e67689af92e912
Max McMillan <walter.hartwell.heisenberg@gmail.com>
2023-09-04 21:38:05 +0200

    retrigger search if number of downloads is smaller than num_results

--- pdf_crawler.py
+++ pdf_crawler.py
@@ -1,14 +1,17 @@
 from googleapiclient.discovery import build
 import requests
-import csv
-import os
+from optparse import OptionParser
 from datetime import datetime
 from email.utils import parsedate_to_datetime
+import csv
+import os
+from urllib.parse import urlparse
 
 class PDFDownloader:
-    def __init__(self, api_key, cse_id, num_results=100, max_size=10000000, cutoff_date=None, csv_file='crawled_pdfs.csv'):
+    def __init__(self, api_key, cse_id, query, num_results=100, max_size=10000000, cutoff_date=None, csv_file='crawled_pdfs.csv'):
         self.api_key = api_key
         self.cse_id = cse_id
+        self.query = query
         self.num_results = num_results
         self.max_size = max_size
         self.cutoff_date = datetime.timestamp(cutoff_date) if cutoff_date else None
@@ -22,63 +25,115 @@ class PDFDownloader:
                 writer = csv.writer(f)
                 writer.writerow(["URL", "Filename"])
 
-    def search_pdfs(self, query):
+    def search_pdfs(self):
+        return self.search_pdfs_until_goal(self.query, self.num_results)
+
+    def is_valid_domain(self, pdf_url, allowed_tlds):
+        parsed_url = urlparse(pdf_url)
+        domain = parsed_url.netloc
+        return any(domain.endswith(tld) for tld in allowed_tlds)
+
+    def is_valid_pdf(self, response):
+        return response.headers.get('Content-Type') == 'application/pdf'
+
+    def is_below_max_size(self, response):
+        size = int(response.headers.get('Content-Length', 0))
+        return self.max_size is None or size <= self.max_size
+
+    def is_newer_than_cutoff(self, last_modified_unix):
+        return self.cutoff_date is None or last_modified_unix >= self.cutoff_date
+
+    def download_single_pdf(self, pdf_url, formatted_date):
+        try:
+            r = requests.get(pdf_url)
+            filename = os.path.join('out', f"{formatted_date}_{pdf_url.split('/')[-1]}")
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+            with open(self.csv_file, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([pdf_url, filename])
+            print(f"{filename} downloaded")
+            return True
+        except requests.exceptions.RequestException:
+            print(f"Error downloading {pdf_url}. Skipping.")
+            return False
+
+    def search_pdfs_until_goal(self, query, goal):
         all_items, start_index = [], 1
-        while len(all_items) < self.num_results:
+        while len(all_items) < goal:
             try:
                 res = self.service.cse().list(q=query, cx=self.cse_id, num=10, start=start_index).execute()
+                if 'items' not in res:
+                    break  # No more search results
                 all_items.extend(res['items'])
                 start_index += 10
             except Exception as e:
                 print(f"Error fetching search results: {e}")
                 break
-        return all_items[:self.num_results]
+        return all_items
 
-    def download_pdfs(self, items):
+    def download_pdfs(self):
+        saved_count, skipped_count, successful_downloads = 0, 0, 0
+        allowed_tlds = ['.de']
+        items = self.search_pdfs()
+
         if not os.path.exists('out'): os.makedirs('out')
-        
-        for item in items:
-            pdf_url = item['link']
-            filename = os.path.join('out', pdf_url.split('/')[-1])
-            try:
-                response = requests.head(pdf_url)
-            except requests.exceptions.RequestException:
-                print(f"Error fetching {pdf_url}. Skipping.")
-                continue
 
-            if response.headers.get('Content-Type') != 'application/pdf':
-                print(f"{pdf_url} is not a valid PDF. Skipping.")
-                continue
+        while successful_downloads < self.num_results:
+            for item in items:
+                if successful_downloads >= self.num_results:
+                    break
+                
+                pdf_url = item['link']
 
-            if self.cutoff_date:
+                if not self.is_valid_domain(pdf_url, allowed_tlds):
+                    print(f"{pdf_url} is not from an allowed domain. Skipping.")
+                    skipped_count += 1
+                    continue
+
                 try:
-                    last_modified = datetime.timestamp(parsedate_to_datetime(response.headers.get('Last-Modified', '')))
-                    if last_modified < self.cutoff_date:
-                        print(f"{pdf_url} is older than the cutoff date. Skipping.")
-                        continue
-                except:
-                    pass
+                    response = requests.head(pdf_url)
+                except requests.exceptions.RequestException:
+                    print(f"Error fetching {pdf_url}. Skipping.")
+                    skipped_count += 1
+                    continue
 
-            size = int(response.headers.get('Content-Length', 0))
-            if self.max_size and size > self.max_size:
-                print(f"{pdf_url} is too large. Skipping.")
-                continue
+                if not self.is_valid_pdf(response):
+                    print(f"{pdf_url} is not a valid PDF. Skipping.")
+                    skipped_count += 1
+                    continue
 
-            try:
-                r = requests.get(pdf_url)
-                with open(filename, 'wb') as f:
-                    f.write(r.content)
-                with open(self.csv_file, 'a', newline='') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([pdf_url, filename])
-                print(f"{filename} downloaded")
-            except requests.exceptions.RequestException:
-                print(f"Error downloading {pdf_url}. Skipping.")
+                last_modified = parsedate_to_datetime(response.headers.get('Last-Modified', ''))
+                last_modified_unix = datetime.timestamp(last_modified)
+
+                if not self.is_newer_than_cutoff(last_modified_unix):
+                    print(f"{pdf_url} is older than the cutoff date. Skipping.")
+                    skipped_count += 1
+                    continue
+
+                if not self.is_below_max_size(response):
+                    print(f"{pdf_url} is too large. Skipping.")
+                    skipped_count += 1
+                    continue
+
+                formatted_date = last_modified.strftime("%Y%m%d")
+                if self.download_single_pdf(pdf_url, formatted_date):
+                    successful_downloads += 1
+                    saved_count += 1
+                else:
+                    skipped_count += 1
+
+            if successful_downloads < self.num_results:
+                new_items = self.search_pdfs_until_goal(self.query, self.num_results - successful_downloads)
+                if not new_items:
+                    break  # No more search results available
+                items = new_items  # Replace old items with newly fetched items
+
+        print(f"Total PDFs saved: {saved_count}")
+        print(f"Total PDFs skipped: {skipped_count}")
+
 
 if __name__ == "__main__":
-    # ... (Argument Parsing here)
-    from optparse import OptionParser
-    from datetime import datetime
     parser = OptionParser()
     parser.add_option("-k", "--key", dest="api_key", help="Google API key")
     parser.add_option("-c", "--cse", dest="cse_id", help="Custom Search Engine ID")
@@ -89,7 +144,6 @@ if __name__ == "__main__":
 
     (options, args) = parser.parse_args()
 
-    # Convert cutoff_date to datetime object if it's not None
     if options.cutoff_date:
         try:
             options.cutoff_date = datetime.strptime(options.cutoff_date, '%Y-%m-%d')
@@ -97,17 +151,6 @@ if __name__ == "__main__":
             print("Invalid date format for cutoff_date. Use YYYY-MM-DD.")
             exit(1)
 
-    # Constructing the query with multiple terms
-    if options.query:
-        search_terms = options.query.split(',')
-        query = " OR ".join([f"{term} filetype:pdf" for term in search_terms])
-    else:
-        print("Query is required.")
-        exit(1)
-
-
-    # Make sure to replace the cutoff_date with a string in 'YYYY-MM-DD' format or None
-    downloader = PDFDownloader(api_key=options.api_key, cse_id=options.cse_id, num_results=options.num_results, max_size=options.max_size, cutoff_date=options.cutoff_date)
-    search_results = downloader.search_pdfs(query)
-    downloader.download_pdfs(search_results)
+    downloader = PDFDownloader(api_key=options.api_key, cse_id=options.cse_id, query=options.query, num_results=options.num_results, max_size=options.max_size, cutoff_date=options.cutoff_date)
+    downloader.download_pdfs()
     print("All PDFs downloaded.")
